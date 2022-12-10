e7d88899831db40c5a59440184ec6b9aac4e5394
0kkun <shinji.okumoto.01@gmail.com>
2022-12-10 17:50:38 +0900

    画像表示のサンプル追加

--- server/resources/js/pages/TopPage.vue
+++ server/resources/js/pages/TopPage.vue
@@ -3,20 +3,25 @@
     <h1>トップページです</h1>
     <h1 class="text-red-400 text-4xl">tailwindテスト</h1>
     <h1>This is from Vue</h1>
-    <div class="vue-content">Hello World from Vue.</div>
+    <div class="top-page-container">Hello World from Vue.</div>
     <div>Count:{{ count }}</div>
-    <button @click="count++">Add Count</button>
+    <button
+      class="bg-green-600 hover:bg-green-500 text-white rounded px-4 py-2"
+      @click="count++"
+    >
+      Add Count
+    </button>
+    <img :src="'/images/no-image.png'" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-
 const count = ref(0)
 </script>
 
 <style lang="scss" scoped>
-.vue-content {
+.top-page-container {
   width: 100%;
   height: 100%;
   background: grey;
